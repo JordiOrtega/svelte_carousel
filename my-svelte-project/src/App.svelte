1b5cdec0ba72dc03f23656296f0646e5aab6b21c
JordiOrtega <jordi.ortega@gmail.com>
2019-06-26 11:04:28 +0200

    Promise modification

--- my-svelte-project/src/App.svelte
+++ my-svelte-project/src/App.svelte
@@ -1,28 +1,23 @@
 <script>
 	export let name;
+    let promise = makeRequest("https://api.flickr.com/services/feeds/photos_public.gne?tags=cat&tagmode=any&format=json");
+        function makeRequest(url){
+          fetch(url,{mode:'no-cors',contentType: 'application/json'}) // Call the fetch function passing the url of the API as a parameter
+            .then(function(response) {
+                // Your code for handling the data you get from the API
+                console.log(response.status, response);
 
-      function makeRequest(url, method) {
-        let request = new XMLHttpRequest();
-        return new Promise(function(resolve, reject) {
-            // Setup listener para procesar la respuesta
-            request.onreadystatechange = function() {
-                // Aseguramos respuesta completa.
-                if (request.readyState !== 4) return;
-                // Procesamos respuesta.
-                request.status >= 200 && request.status < 300 ?
-                    resolve(request) :
-                    reject({
-                        status: request.status,
-                        statusText: request.statusText
-                    });
-            };
-            request.open(method || "GET", url, true);
-            request.send();
-        });
-    };
+            })
+            .catch(function() {
+                // This is where you run code if the server returns any errors
+                console.log('error');
+            });
+        }
 	function handleClick() {
-		let promise = makeRequest(https://api.jetlore.com/layouts/layout.json?cid=aacd9f9a13d8541db672d814e93f47b3&id=&&feed=bershka_all&div=null&lang=null&layout=Personalization_carrusel_rebajas&secure=false&jl_add_info=false&jl_backfill=yes);
-	}
+        //promise = makeRequest("https://api.jetlore.com/layouts/layout.json?cid=aacd9f9a13d8541db672d814e93f47b3&id=&&feed=bershka_all&div=null&lang=null&layout=PruebaCaro111&secure=true&jl_add_info=true&jl_response=full&jl_backfill=yes");
+		promise = makeRequest("https://api.flickr.com/services/feeds/photos_public.gne?tags=tsuboniwa,garden&format=json");
+        
+    }
 </script>
 
 <button on:click={handleClick}>
